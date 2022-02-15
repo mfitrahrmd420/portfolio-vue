92dfebc6e389637191e3ac97830d64a2318bf1c4
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 03:37:58 +0700

    edit nav links

--- src/components/base/Navbar.vue
+++ src/components/base/Navbar.vue
@@ -1,18 +1,43 @@
 <template>
-  <nav ref="nav" class="z-50 w-full p-2 lg:px-10 lg:py-3 bg-primary-500 transform flex flex-row items-center fixed top-0 left-0 shadow-lg">
+  <nav
+    ref="nav"
+    class="z-50 w-full p-2 lg:px-10 lg:py-3 bg-primary-500 transform flex flex-row items-center fixed top-0 left-0 shadow-lg"
+  >
     <div class="w-24 h-14">
-      <img class="object-cover h-full w-full" src="@/assets/signature.png" alt="" />
+      <img
+        class="object-cover h-full w-full"
+        src="@/assets/signature.png"
+        alt=""
+      />
     </div>
     <div class="mx-auto"></div>
     <ul class="hidden md:flex flex-row space-x-3">
-      <li><BaseButton href="#home">Home</BaseButton></li>
-      <li><BaseButton href="#about">About</BaseButton></li>
-      <li><BaseButton href="#project">Project</BaseButton></li>
-      <li><BaseButton href="#design">Design</BaseButton></li>
-      <li><BaseButton href="#contact">Contact</BaseButton></li>
+      <li>
+        <BaseButton href="#home">Home</BaseButton>
+      </li>
+      <li>
+        <BaseButton href="#about">About</BaseButton>
+      </li>
+      <li>
+        <BaseButton href="#project">Project</BaseButton>
+      </li>
+      <li>
+        <BaseButton href="#design">Design</BaseButton>
+      </li>
+      <li>
+        <BaseButton href="#contact">Contact</BaseButton>
+      </li>
     </ul>
-    <div class="inline-block md:hidden px-4 py-1 rounded-sm hover:bg-accent-1 hover:text-black transition-colors duration-200 cursor-pointer" @click="$emit('menuOn')">Menu</div>
-    <div class="ml-5"><BaseButton class="border">Resume</BaseButton></div>
+    <div
+      class="inline-block md:hidden px-4 py-1 rounded-sm hover:bg-accent-1 hover:text-black transition-colors duration-200 cursor-pointer"
+      @click="$emit('menuOn')"
+    >Menu</div>
+    <a
+      href="https://drive.google.com/u/0/uc?id=1OacPXGJGHkr9rkFgUrt8U2zHujRA26nJ&export=download"
+      class="ml-5"
+    >
+      <BaseButton class="border">Resume</BaseButton>
+    </a>
   </nav>
 </template>
 
